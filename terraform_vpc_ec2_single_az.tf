ee796538eb1cca4a86ce7befdf6934a49d593c88
AmitShemesh <45431917+AmitShemeshCrowd@users.noreply.github.com>
2022-10-20 14:50:01 +0300

    Update terraform_vpc_ec2_single_az.tf

--- terraform_vpc_ec2_single_az.tf
+++ terraform_vpc_ec2_single_az.tf
@@ -135,13 +135,26 @@ resource "local_file" "aws_cloud_pem" {
   content = tls_private_key.example.private_key_pem
 }
 
-resource "aws_instance" "testInstance" {
-#   ami           = var.instance_ami
-  instance_type = var.instance_type
-  subnet_id = aws_subnet.subnet_public.id
-  vpc_security_group_ids = [aws_security_group.sg_22.id]
-  key_name = aws_key_pair.generated_key.key_name
- tags = {
-  Environment = var.environment_tag
- }
+resource "aws_ami" "main" {
+  name                = "amit-ami"
+  virtualization_type = "hvm"
+  root_device_name    = "/dev/xvda"
+  imds_support        = "v2.0" # Enforce usage of IMDSv2. You can safely remove this line if your application explicitly doesn't support it.
+  ebs_block_device {
+    device_name = "/dev/xvda"
+    snapshot_id = "snap-xxxxxxxx"
+    volume_size = 8
+  }
 }
+
+
+# resource "aws_instance" "testInstance" {
+#   ami           = var.instance_ami
+#   instance_type = var.instance_type
+#   subnet_id = aws_subnet.subnet_public.id
+#   vpc_security_group_ids = [aws_security_group.sg_22.id]
+#   key_name = aws_key_pair.generated_key.key_name
+#  tags = {
+#   Environment = var.environment_tag
+#  }
+# }

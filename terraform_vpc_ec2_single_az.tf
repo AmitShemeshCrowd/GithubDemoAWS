00257fbac2af59a076631023db63357072b02e7c
AmitShemesh <45431917+AmitShemeshCrowd@users.noreply.github.com>
2022-10-20 13:57:01 +0300

    Update terraform_vpc_ec2_single_az.tf

--- terraform_vpc_ec2_single_az.tf
+++ terraform_vpc_ec2_single_az.tf
@@ -6,18 +6,10 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
+      source = "hashicorp/aws"
+      version = "3.70.0"
     }
   }
-
-  required_version = ">= 1.2.0"
-}
-
-
-
-provider "aws" {
-  region = "us-east-1"
 }
 
 

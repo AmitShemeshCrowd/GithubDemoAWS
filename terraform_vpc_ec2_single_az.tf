1ee4c4d1e3e2be4aff68f095562d2810a1da35be
AmitShemesh <45431917+AmitShemeshCrowd@users.noreply.github.com>
2022-10-20 14:40:49 +0300

    Update terraform_vpc_ec2_single_az.tf

--- terraform_vpc_ec2_single_az.tf
+++ terraform_vpc_ec2_single_az.tf
@@ -72,7 +72,7 @@ resource "aws_subnet" "subnet_public" {
   vpc_id = aws_vpc.vpc.id
   cidr_block = var.cidr_subnet
   map_public_ip_on_launch = "true"
-  availability_zone = us-east-1a
+  availability_zone = ['us-east-1a']
   tags = {
     Environment = var.environment_tag
   }

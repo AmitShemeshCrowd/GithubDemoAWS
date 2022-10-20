80941a1840198c83fc0f02c098336eb86e8ee982
AmitShemesh <45431917+AmitShemeshCrowd@users.noreply.github.com>
2022-10-20 15:08:32 +0300

    Update terraform_vpc_ec2_single_az.tf

--- terraform_vpc_ec2_single_az.tf
+++ terraform_vpc_ec2_single_az.tf
@@ -139,9 +139,9 @@ resource "local_file" "aws_cloud_pem" {
 resource "aws_instance" "testInstance" {
   ami           = var.instance_ami
   instance_type = var.instance_type
-  subnet_id = aws_subnet.subnet_public.id
-  vpc_security_group_ids = [aws_security_group.sg_22.id]
-  key_name = aws_key_pair.generated_key.key_name
+#   subnet_id = aws_subnet.subnet_public.id
+#   vpc_security_group_ids = [aws_security_group.sg_22.id]
+#   key_name = aws_key_pair.generated_key.key_name
  tags = {
   Environment = var.environment_tag
  }
